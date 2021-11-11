a8bfbbfc22281b6de4e6d27a326a36509603eaad
AlvaroAquijeDiaz <alvarodevcode@outlook.com>
2021-11-11 17:17:19 -0500

    feat(components): :sparkles: added features and functionalities

--- components/Layout/SideBar.tsx
+++ components/Layout/SideBar.tsx
@@ -1,14 +1,63 @@
 import { NextPage } from "next";
-import { STYLE_DIMENSIONS } from "../../utils/constants/STYLE_DIMENSIONS";
+import { CustomLink } from "../CustomLink";
+import * as HeroIcons from "@heroicons/react/outline";
+import { CustomDropdownMenuBtn } from "../CustomDropdownMenuBtn";
 
 type Props = {};
+
+const links = [
+  {
+    href: "/",
+    label: "Home",
+    icon: <HeroIcons.HomeIcon className="w-4" />,
+  },
+  {
+    href: "/myFiles",
+    label: "My Files",
+    icon: <HeroIcons.DatabaseIcon className="w-4" />,
+  },
+  {
+    href: "/recentFiles",
+    label: "Recent Files",
+    icon: <HeroIcons.RefreshIcon className="w-4" />,
+  },
+  {
+    href: "/sharedFiles",
+    label: "Shared Files",
+    icon: <HeroIcons.ShareIcon className="w-4" />,
+  },
+  {
+    href: "/fileRequest",
+    label: "File Request",
+    icon: <HeroIcons.ExclamationCircleIcon className="w-4" />,
+  },
+  {
+    href: "/trash",
+    label: "Trash",
+    icon: <HeroIcons.TrashIcon className="w-4" />,
+  },
+];
 
 export const SideBar: NextPage<Props> = () => {
   return (
     <aside
-      className={`bg-bgSideBar flex flex-col space-y-5 py-8 fixed inset-0 w-${STYLE_DIMENSIONS.NAVIGATION_SIDEBAR_WIDTH}`}
+      className={`hidden bg-bgSideBar md:flex md:flex-col md:justify-between fixed inset-0 md:w-[clamp(192px,15vw,202px)] `}
     >
-      <h1>Sidebar</h1>
+      <ul className="flex flex-col space-y-5 pt-28 pb-8 border-[#464C61] border-b">
+        {links.map(({ href, icon, label }) => (
+          <li key={href}>
+            <CustomLink
+              label={label}
+              url={href}
+              Icon={icon}
+              extraClassnames="text-white capitalize text-sm"
+            />
+          </li>
+        ))}
+      </ul>
+      <div className="flex mb-10 justify-center">
+        <CustomDropdownMenuBtn />
+      </div>
     </aside>
   );
 };
---
+++ pages/trash/index.tsx
@@ -0,0 +1,10 @@
+import { NextPage } from "next";
+import React from "react";
+
+type Props = {};
+
+const TrashPage: NextPage<Props> = () => {
+  return <div>Trash</div>;
+};
+
+export default TrashPage;
