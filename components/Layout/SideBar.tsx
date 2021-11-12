d54d14427403a0cb4ea8b14adf576c8c636c406f
AlvaroAquijeDiaz <alvarodevcode@outlook.com>
2021-11-11 20:38:00 -0500

    fix(components): :bug: fixed radix id provider error

--- components/Layout/SideBar.tsx
+++ components/Layout/SideBar.tsx
@@ -1,42 +1,19 @@
 import { NextPage } from "next";
 import { CustomLink } from "../CustomLink";
-import * as HeroIcons from "@heroicons/react/outline";
-import { CustomDropdownMenuBtn } from "../CustomDropdownMenuBtn";
+// import { CustomDropdownMenuBtn } from "../CustomDropdownMenuBtn";
+import { PAGE_LINKS } from "../../utils/LINKS";
+
+import dynamic from "next/dynamic";
+
+// ? This solves a Radix error
+const DynamicDropdownMenu = dynamic<{}>(
+  import("../CustomDropdownMenuBtn/index").then(
+    (mod) => mod.CustomDropdownMenuBtn
+  ),
+  { ssr: false }
+);
 
 type Props = {};
-
-const links = [
-  {
-    href: "/",
-    label: "Home",
-    icon: <HeroIcons.HomeIcon className="w-4" />,
-  },
-  {
-    href: "/myFiles",
-    label: "My Files",
-    icon: <HeroIcons.DatabaseIcon className="w-4" />,
-  },
-  {
-    href: "/recentFiles",
-    label: "Recent Files",
-    icon: <HeroIcons.RefreshIcon className="w-4" />,
-  },
-  {
-    href: "/sharedFiles",
-    label: "Shared Files",
-    icon: <HeroIcons.ShareIcon className="w-4" />,
-  },
-  {
-    href: "/fileRequest",
-    label: "File Request",
-    icon: <HeroIcons.ExclamationCircleIcon className="w-4" />,
-  },
-  {
-    href: "/trash",
-    label: "Trash",
-    icon: <HeroIcons.TrashIcon className="w-4" />,
-  },
-];
 
 export const SideBar: NextPage<Props> = () => {
   return (
@@ -44,7 +21,7 @@ export const SideBar: NextPage<Props> = () => {
       className={`hidden bg-bgSideBar md:flex md:flex-col md:justify-between fixed inset-0 md:w-[clamp(192px,15vw,202px)] `}
     >
       <ul className="flex flex-col space-y-5 pt-28 pb-8 border-[#464C61] border-b">
-        {links.map(({ href, icon, label }) => (
+        {PAGE_LINKS.map(({ href, icon, label }) => (
           <li key={href}>
             <CustomLink
               label={label}
@@ -56,7 +33,8 @@ export const SideBar: NextPage<Props> = () => {
         ))}
       </ul>
       <div className="flex mb-10 justify-center">
-        <CustomDropdownMenuBtn />
+        {/* <CustomDropdownMenuBtn /> */}
+        <DynamicDropdownMenu />
       </div>
     </aside>
   );
