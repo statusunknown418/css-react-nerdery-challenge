b8228afe38c296725d789e8d273ace9648d6925f
AlvaroAquijeDiaz <alvarodevcode@outlook.com>
2021-11-12 13:14:11 -0500

    style(components): :lipstick: minor styling updates

--- components/HomeComponents/RecentFiles/index.tsx
+++ components/HomeComponents/RecentFiles/index.tsx
@@ -8,9 +8,9 @@ export const RecentFilesSection: NextPage<Props> = () => {
   return (
     <section className="my-5">
       <h2 className="font-bold text-lg">Recent Files</h2>
-      <header className="flex flex-col">
+      <header className="flex flex-col my-2">
         <ul className="grid grid-cols-2 place-items-center justify-items-start gap-2 text-[#858A9D] text-xs sm:grid-cols-4">
-          <li className="pl-3 sm:col-span-2 ">Name</li>
+          <li className="pl-2 sm:col-span-2 ">Name</li>
           <li className=" hidden sm:block">Members</li>
           <li className="place-self-end sm:place-self-auto pr-2">
             Last Modified
