6121ca008e1bfe00093c3e87812a8db58cb99968
AlvaroAquijeDiaz <alvarodevcode@outlook.com>
2021-11-12 13:11:10 -0500

    refactor(components): extracted common components into a folder

---
+++ components/HomeComponents/AsideDetails.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+
+interface Props {}
+
+export const AsideDetails: NextPage<Props> = () => {
+  return (
+    <div>
+      <h2 className="font-bold">Aside with Storage details</h2>
+    </div>
+  );
+};
