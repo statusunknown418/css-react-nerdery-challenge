2c869319034b9f6011fa08026c10b7b1b03e8d4c
AlvaroAquijeDiaz <alvarodevcode@outlook.com>
2021-11-12 13:11:41 -0500

    feat(utils): :building_construction: extracted constants

---
+++ utils/PROJECTS.tsx
@@ -0,0 +1,28 @@
+import { TProjectCardArr } from "../components/HomeComponents/ProjectCard";
+
+export const PROJECTS: TProjectCardArr = [
+  {
+    id: 1,
+    title: "app project",
+    createdAt: "2020-01-01",
+    nOfDocs: 2,
+  },
+  {
+    id: 2,
+    title: "project: fitbit",
+    createdAt: "2020-01-02",
+    nOfDocs: 2,
+  },
+  {
+    id: 3,
+    title: "client documents",
+    createdAt: "2020-01-03",
+    nOfDocs: 5,
+  },
+  {
+    id: 4,
+    title: "client documents",
+    createdAt: "2020-01-03",
+    nOfDocs: 10,
+  },
+];
