ba878ff347d2fe993e654e9509c1769525f10ed4
alvarotech <alvarodevcode@outlook.com>
2021-11-15 16:34:48 -0500

    fix(utils): :building_construction: updated constants

---
+++ utils/SEED_USER.tsx
@@ -0,0 +1,8 @@
+import { IAsideDetailsProps } from "../components/HomeComponents/AsideDetails";
+
+export const SEED_USER: IAsideDetailsProps = {
+  name: "John Doe",
+  email: "john@doe.com",
+  profilePicUrl: "https://randomuser.me/api/portraits/thumb/men/75.jpg",
+  tier: "pro",
+};
